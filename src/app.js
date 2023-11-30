bfbdfd5670f295e318b2c332e8d5f701302e3248
iqbalfadhila <iqbalfadhila8902@gmail.com>
2023-11-30 21:04:39 +0700

    port

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 
