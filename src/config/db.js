b63ffdeab01d0fefa66d6554fa13fcbe2c2a029d
iqbalfadhila <iqbalfadhila8902@gmail.com>
2023-12-02 14:23:05 +0700

    deploy

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,29 @@
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 dotenv.config();
-const config = require('./config.json')[process.env.NODE_ENV || 'development'];
+// const config = require('./config.json')[process.env.NODE_ENV || 'development'];
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME || config.database,
-  process.env.DB_USER || config.username,
-  process.env.DB_PASSWORD || config.password,
-  {
-    dialect: 'mysql',
-    dialectOptions: {
-      socketPath: "/cloudsql/jude:us-central1:testdbjude"
-    }
-  }
-);
+const config = {
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.INSTANCE_UNIX_SOCKET,
+  dialect: 'mysql',
+}
+
+const sequelize = new Sequelize(config);
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME || config.database,
+//   process.env.DB_USER || config.username,
+//   process.env.DB_PASSWORD || config.password,
+//   {
+//     dialect: 'mysql',
+//     dialectOptions: {
+//       socketPath: "/cloudsql/jude:us-central1:testdbjude"
+//     }
+//   }
+// );
 
 sequelize.authenticate()
   .then(() => {
