e427e5cc3c511aa4acf6b8fa516a132d83d81bea
iqbalfadhila <iqbalfadhila8902@gmail.com>
2023-12-05 14:33:56 +0700

    photo user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,41 +1,92 @@
 const User = require("../models/User");
+const { Storage } = require('@google-cloud/storage');
+const path = require('path');
+const multer = require('multer');
+
+const storage = new Storage({
+  keyFilename: path.join(__dirname, '../config/serviceAccountKey.json'), // Replace with the path to your key file
+  projectId: 'jude-406606', // Replace with your Google Cloud Project ID
+});
+
+const bucket = storage.bucket('bucket-jude-406606'); // Replace with your Google Cloud Storage bucket name
+
+// const imageFilter = (req, file, cb) => {
+//   if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+//     return cb(new Error('Hanya file gambar yang diperbolehkan!'), false);
+//   }
+//   cb(null, true);
+// };
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  // limits: {
+  //   fileSize: 2 * 1024 * 1024, // Batas 2 MB
+  // },
+  // fileFilter: imageFilter,
+});
+
 const updateUser = async (req, res) => {
-  const userId = req.user.id;
-
-  // Extract the fields you want to update from the request body
-  const { username, email, fullname, phone, gender, date_of_birth, photo } = req.body;
-
-  // Validasi bahwa setidaknya satu field harus diisi untuk melakukan update
-  if (!username && !email && !fullname && !phone && !gender && !date_of_birth && !photo) {
-    return res.status(400).json({ message: 'At least one field must be filled in for update.' });
-  }
-
   try {
-    // Cek apakah user dengan ID tersebut ada
+    const userId = req.user.id;
+    const { username, email, fullname, phone, gender, date_of_birth } = req.body;
+
     const existingUser = await User.findByPk(userId);
 
     if (!existingUser) {
       return res.status(404).json({ message: 'User not found.' });
     }
 
-    // Update user data termasuk foto jika diunggah
-    await existingUser.update({
+    const updatedUserData = {
       username: username || existingUser.username,
       email: email || existingUser.email,
       fullname: fullname || existingUser.fullname,
       phone: phone || existingUser.phone,
       gender: gender || existingUser.gender,
       date_of_birth: date_of_birth || existingUser.date_of_birth,
-      photo: req.file ? req.file.filename : existingUser.photo, // Gunakan nama file baru jika foto diunggah
-    });
+    };
 
-    res.json({ message: 'User data updated successfully.' });
+    if (req.file) {
+      const file = req.file;
+      
+      // Validasi format file
+      if (!file.mimetype.startsWith('image/')) {
+        return res.status(400).json({ message: 'Hanya file gambar yang diperbolehkan!' });
+      }
+      // Validasi ukuran file
+      if (file.size > 2 * 1024 * 1024) {
+        return res.status(400).json({ message: 'Ukuran file foto melebihi batas 2 MB.' });
+      }
+
+      // Ubah ini menjadi folder yang diinginkan, misalnya "profileuser"
+      const folderName = "profileuser";
+
+      const fileName = `${folderName}/user_${userId}_${Date.now()}${path.extname(file.originalname)}`;
+      const fileBuffer = file.buffer;
+
+      const blob = bucket.file(fileName);
+      const blobStream = blob.createWriteStream();
+
+      blobStream.on('error', (err) => {
+        console.error('Error uploading file to Google Cloud Storage:', err);
+        res.status(500).json({ error: 'Error uploading file to Google Cloud Storage' });
+      });
+
+      blobStream.on('finish', async () => {
+        updatedUserData.photo = fileName;
+        await existingUser.update(updatedUserData);
+        res.json({ message: 'User data updated successfully.' });
+      });
+
+      blobStream.end(fileBuffer);
+    } else {
+      await existingUser.update(updatedUserData);
+      res.json({ message: 'User data updated successfully.' });
+    }
   } catch (error) {
     console.error('Error updating user:', error.message);
     res.status(500).json({ message: 'Internal Server Error' });
   }
 };
-
 
 const getAllUser = async (req, res) => {
   try {
@@ -85,9 +136,37 @@ const deleteUser = async (req, res) => {
   }
 };
 
+const getUserPhotoUrl = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const existingUser = await User.findByPk(userId);
+    console.log(existingUser);
+
+    if (!existingUser) {
+      return res.status(404).json({ message: 'User tidak ada.' });
+    }
+
+    const photoUrl = existingUser.photo;
+
+    if (!photoUrl) {
+      return res.status(404).json({ message: 'User photo not found.' });
+    }
+
+    // Ganti URL bucket dan path sesuai dengan struktur penyimpanan Anda
+    const photoStorageUrl = `https://storage.googleapis.com/bucket-jude-406606/${photoUrl}`;
+
+    res.json({ photoUrl: photoStorageUrl });
+  } catch (error) {
+    console.error('Error getting user photo:', error.message);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
 module.exports = {
   updateUser,
+  upload,
   getAllUser,
   getUserById,
   deleteUser,
+  getUserPhotoUrl,
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,27 +3,17 @@ const multer = require('multer');
 const path = require('path');
 const router = express.Router();
 const authenticateToken = require('../middleware/authenticateToken');
-const { updateUser, getAllUser, getUserById, deleteUser } = require('../controllers/userController');
+const authorizeRole = require('../middleware/authorizeRole');
+const { updateUser, upload, getAllUser, getUserById, deleteUser, getUserPhotoUrl } = require('../controllers/userController');
 
-// Tentukan tempat penyimpanan dan nama file
-const storage = multer.diskStorage({
-destination: (req, file, cb) => {
-    cb(null, 'src/uploads'); // Simpan file di folder 'uploads'
-},
-filename: (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    cb(null, 'user_' + Date.now() + ext); // Nama file: user_timestamp
-},
-});
-
-// Konfigurasi multer
-const upload = multer({ storage: storage });
+// const storage = multer.memoryStorage();
+// const upload = multer({ storage });
 
 // Menyajikan file gambar yang diunggah
-router.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+// router.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
-// Route untuk mengupdate user
-router.put('/update', upload.single('photo'), updateUser);
+router.put('/update', authenticateToken, authorizeRole('user'), upload.single('photo'), updateUser);
+router.get('/photo', authenticateToken, getUserPhotoUrl); // Tambahkan endpoint ini
 router.get('/', authenticateToken, getAllUser);
 router.get('/:id', authenticateToken, getUserById);
 router.delete('/:id', authenticateToken, deleteUser);
