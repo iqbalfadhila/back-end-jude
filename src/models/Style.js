67295a705cfb656cccea06508643fd81aa3533db
iqbalfadhila <iqbalfadhila8902@gmail.com>
2023-12-09 00:09:50 +0700

    fix bug

--- src/models/Style.js
+++ src/models/Style.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 
@@ -24,33 +24,4 @@ const Style = sequelize.define('styles', {
         defaultValue: DataTypes.NOW,
     }
 });
-
-=======
-const { DataTypes } = require("sequelize");
-const sequelize = require("../config/db");
-
-const Style = sequelize.define('styles', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
-    },
-    style_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    createdAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW,
-    },
-    updatedAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW,
-    }
-});
-
->>>>>>> a1de2a0618c3f9501131560ed1bb04cff20dd316
 module.exports = Style;
